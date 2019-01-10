9c540df3fb7ea157a1da4147db101d923088b2ab
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-10 19:34:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,5 +1,8 @@
 def instantiate_new_array
   instantiate_new_array = []
   @my_new_array = instantiate_new_array
+end
 
+def array_with_two_elements
+  @my_two_array = ["gilmore girls", "house of cards"]
 end
